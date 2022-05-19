c453eefed522599c4859239667cc8cbe39fe495d
priority <1832052104@qq.com>
2022-05-19 23:51:33 +0800

    fix:promise 完善

--- 2. promise/promise.js
+++ 2. promise/promise.js
@@ -1,94 +1,107 @@
-// state promise state 3 : pending/rej
-const PENDING = 'pending'
-const FULFILLED = 'fulfilled'
-const REJECTED = 'rejected'
-
-class myPromise {
-  // promise 传入的执行回调
-
-  constructor(handle) {
-    if (!this._isFunction(handle)) {
-      throw Error('please pass in the correct function~~~')
+;(function (root, factory) {
+  if (typeof module === 'object') {
+    // 即为node环境
+    module.exports = factory()
+  } else {
+    // browser 挂在到window上
+    window['Promise'] = factory()
+  }
+})(this, function () {
+  const PENDING = 'pending'
+  const FULFILLED = 'fulfilled'
+  const REJECTED = 'rejected'
+  class myPromise {
+    // promise 接受一个函数参数
+    constructor(handle) {
+      this.initValue()
+      try {
+        // try 处理了当handle不为函数的情况,
+        // 同时bind绑定到实例上,防止在调用的时候随着函数执行环境this丢失
+        handle(this._resolve.bind(this), this._reject.bind(this))
+      } catch (err) {
+        this._reject(err)
+      }
     }
-    this.initValue()
-    // 初始化resolve绑定到实例上,防止随着函数执行环境的改变而改变
-    handle(this.resolve.bind(this), this.reject.bind(this))
-  }
-  _isFunction(handle) {
-    return typeof handle === 'function'
-  }
-
-  initValue() {
-    // value 为保存的结果值
-    this.value = undefined
-    this.state = PENDING
-    // resolve 或者 reject 时要把所有通过 .then 和 .catch 产生的新的 promise 的 state 也修改一下
-    // 到时候直接执行这些 callbacks 就行了
-    this.callback = []
-  }
-  resolve(data) {
-    // 可以直接if判断，这里必须为箭头函数否则this为undefined
-    this.state === PENDING &&
-      (() => {
-        // value 保存结果值
-        this.value = data
-        this.state = FULFILLED
-        // 在注册表中去执行回调
-        // 同一promise对象可以添加多个then监听，状态改变时按照注册顺序依次执行
-        this.callback.forEach(fn => fn(this.value))
-        // queueMicrotask(() => {
-        //   this.onResolvedCallbacks.forEach(fn => fn(this.value))
-        // })
-      })()
-  }
-  reject(data) {
-    this.state === PENDING &&
-      (() => {
-        this.value = data
-        this.state = REJECTED
-        // 同上
-        this.onRejectedCallbacks.forEach(fn => fn(this.value))
-      })()
-  }
-  // then返回也可以是一个promise
-  then(onFulfilled, onRejected) {
-    return new myPromise((nextResolve, nextReject) => {
-      let callback = () => {
-        // .then .catch 均为微任务 添加到微任务队列
+    _isFunction(fn) {
+      return typeof fn === 'function'
+    }
+    // 初始化相关
+    initValue() {
+      // 初始化状态
+      this.state = PENDING
+      // 初始化值 .resolve .reject时的值
+      this.value = undefined
+      // 存放.then .catch时候的函数,
+      this.callback = []
+    }
+    // resolve 接受一个值 在then中可以拿到，而这个值我们就可以保存在this.value中
+    _resolve(data) {
+      this.state === PENDING &&
+        (() => {
+          this.value = data
+          this.state = FULFILLED
+          // queueMicrotask(() => {
+          //   this.callback(this.value)
+          // })
+          this.callback.forEach(fn => fn())
+        })()
+    }
+    _reject(data) {
+      this.state === PENDING &&
+        (() => {
+          this.value = data
+          this.state = REJECTED
+          // queueMicrotask(() => {
+          //   this.callback(this.value)
+          // })
+          this.callback.forEach(fn => fn())
+        })()
+    }
+    // .then当中接受两个回调，我们就暂时把resolve存放在this.callback上
+    then(onFulfilled, onRejected) {
+      let resolve, reject
+      const newPromise = new myPromise((nextResolve, nextReject) => {
+        resolve = nextResolve
+        reject = nextReject
+      })
+      const callback = () => {
         queueMicrotask(() => {
-          let newResult
-          if (this.state === FULFILLED) {
-            // 对于.then((res) => {}) 和 .then()
-            newResult = this._isFunction(onFulfilled)
-              ? nextResolve(onFulfilled(this.value))
-              : nextResolve(this.value)
-          } else if (typeof onRejected === 'function') {
-            // 对应于 .catch(err => xxx)   ------- 对应于 .catch() 或者直接没有 .catch 时
-            newResult = onRejected(this.value)
-          } else {
-            nextReject(this.value)
-            return
-          }
-          // 结果同样是一个promise
-          if (newResult instanceof myPromise) {
-            // TODO: ⑧理解
-            // 这种时候是这个返回的 promise 的 state 决定 newPromise 的 state
-            newResult.then()
-          } else {
-            nextResolve(newResult)
+          try {
+            let newResult
+            if (this.state === FULFILLED) {
+              // 此时走的是_resolve
+              newResult = this._isFunction(onFulfilled) ? onFulfilled(this.value) : this.value
+            } else if (this._isFunction(onRejected)) {
+              // 在这之后走的都是_reject 对应于 .catch(err => xxx)
+              newResult = onRejected(this.value)
+            } else {
+              // 当reject出来之后没有参数接受时 对应于 .catch() 或者直接没有 .catch 时
+              reject(this.value)
+              return
+            }
+            if (newPromise === newResult) {
+              throw TypeError('is same~~~')
+            }
+            if (newResult instanceof myPromise) {
+              newResult.then(resolve, reject)
+            } else {
+              resolve(newResult)
+            }
+          } catch (err) {
+            reject(err)
           }
         })
       }
-
-      //
       if (this.state === PENDING) {
+        // 说明还没到resolve或者reject 那么直接push到callback当中
         this.callback.push(callback)
       } else {
+        // 执行当前then的callback
         callback()
       }
-    })
+
+      return newPromise
+    }
   }
-  catch(onRejected) {
-    return this.then(undefined, onRejected)
-  }
-}
+  return myPromise
+})
